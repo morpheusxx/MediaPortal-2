1ff53b3ff7d9bbba5cb4d31ebfb4b36fe25ffee4
Baboonanza <mrjtuk@gmail.com>
2013-03-25 17:20:06 +0100

    Fixed opacity brush rendering:
     - Element render transforms no longer affect opacity brushes
     - Special opacity handling in brushes no longer required
     - Effects and opacity brushes can now be used together

--- MediaPortal/Source/UI/SkinEngine/Controls/Brushes/VideoBrush.cs
+++ MediaPortal/Source/UI/SkinEngine/Controls/Brushes/VideoBrush.cs
@@ -348,11 +348,6 @@ namespace MediaPortal.UI.SkinEngine.Controls.Brushes
       return player != null;
     }
 
-    protected override bool BeginRenderOpacityBrushOverride(Texture tex, RenderContext renderContext)
-    {
-      throw new NotImplementedException("VideoBrush doesn't support being rendered as an opacity brush");
-    }
-
     public override void EndRender()
     {
       _imageContext.EndRender();

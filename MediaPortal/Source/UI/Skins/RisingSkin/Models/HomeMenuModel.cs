c619d0d1129a479d2dbd1897e69ee75e2745036d
morpheusxx <morpheus_xx@team-mediaportal.com>
2016-02-01 20:51:30 +0100

    Next try: 2 separated list views, synced in model

--- MediaPortal/Source/UI/Skins/RisingSkin/Models/HomeMenuModel.cs
+++ MediaPortal/Source/UI/Skins/RisingSkin/Models/HomeMenuModel.cs
@@ -24,8 +24,14 @@
 
 using System;
 using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Windows.Forms;
+using MediaPortal.Common.General;
 using MediaPortal.UiComponents.SkinBase.General;
 using MediaPortal.UI.Presentation.DataObjects;
+using MediaPortal.UI.SkinEngine.MpfElements;
+using MediaPortal.Utilities;
 
 namespace MediaPortal.UiComponents.RisingSkin.Models
 {
@@ -43,13 +49,152 @@ namespace MediaPortal.UiComponents.RisingSkin.Models
     public ItemsList SubItems { get; private set; }
   }
 
+
+  /// <summary>
+  /// <see cref="NavigationList{T}"/> provides navigation features for moving inside a <see cref="List{T}"/> and exposing <see cref="Current"/> item.
+  /// </summary>
+  /// <typeparam name="T"></typeparam>
+  public class NavigationList<T> : List<T>, IObservable
+  {
+    public delegate void CurrentChangedEvent(int oldIndex, int newIndex);
+    public CurrentChangedEvent OnCurrentChanged;
+    public EventHandler OnListChanged;
+    protected WeakEventMulticastDelegate _objectChanged = new WeakEventMulticastDelegate();
+
+    /// <summary>
+    /// Event which gets fired when the collection changes.
+    /// </summary>
+    public event ObjectChangedDlgt ObjectChanged
+    {
+      add { _objectChanged.Attach(value); }
+      remove { _objectChanged.Detach(value); }
+    }
+
+    public void FireChange()
+    {
+      _objectChanged.Fire(new object[] { this });
+    }
+
+    private int _current;
+
+    public T Current
+    {
+      get { return Count > 0 && _current < Count ? this[_current] : default(T); }
+    }
+
+    public int CurrentIndex
+    {
+      get { return Count > 0 ? _current : -1; }
+      set
+      {
+        if (Count == 0 || value < 0 || value >= Count)
+          return;
+        int oldIndex = CurrentIndex;
+        _current = value;
+        FireCurrentChanged(oldIndex);
+      }
+    }
+
+    public void MoveNext()
+    {
+      if (Count == 0)
+        return;
+      int oldIndex = CurrentIndex;
+      _current++;
+      if (_current >= Count)
+        _current = 0;
+      FireCurrentChanged(oldIndex);
+    }
+
+    public void MovePrevious()
+    {
+      if (Count == 0)
+        return;
+      int oldIndex = CurrentIndex;
+      _current--;
+      if (_current < 0)
+        _current = Count - 1;
+      FireCurrentChanged(oldIndex);
+    }
+
+    public void SetIndex(int index)
+    {
+      if (Count == 0 || index < 0 || index >= Count)
+        return;
+      int oldIndex = CurrentIndex;
+      _current = index;
+      FireCurrentChanged(oldIndex);
+    }
+
+    public bool MoveTo(Predicate<T> condition)
+    {
+      int oldIndex = CurrentIndex;
+      for (int index = 0; index < Count; index++)
+      {
+        T item = this[index];
+        if (!condition.Invoke(item))
+          continue;
+        _current = index;
+        return true;
+      }
+      FireCurrentChanged(oldIndex);
+      return false;
+    }
+
+    public void FireCurrentChanged(int oldIndex)
+    {
+      var currentIndex = CurrentIndex;
+      if (OnCurrentChanged != null && oldIndex != currentIndex)
+        OnCurrentChanged(oldIndex, currentIndex);
+    }
+
+    public void FireListChanged()
+    {
+      if (OnListChanged != null)
+        OnListChanged(this, EventArgs.Empty);
+    }
+  }
+
   public class HomeMenuModel
   {
-    public ItemsList MenuItems { get; private set; }
+    public NavigationList<NestedItem> MenuItems { get; private set; }
+    public ItemsList SubItems { get; private set; }
+
+    public void MoveNext()
+    {
+      MenuItems.MoveNext();
+    }
+
+    public void MovePrevious()
+    {
+      MenuItems.MovePrevious();
+    }
+
+    public void SetSelectedItem(object sender, SelectionChangedEventArgs e)
+    {
+      var item = e.FirstAddedItem as NestedItem;
+      SetSubItems(item);
+    }
+
+    private void SetSubItems(NestedItem item)
+    {
+      if (item != null)
+      {
+        SubItems.Clear();
+        CollectionUtils.AddAll(SubItems, item.SubItems);
+        SubItems.FireChange();
+      }
+    }
+
+    public void OnKeyPress(object sender, KeyPressEventArgs e)
+    {
+      
+    }
 
     public HomeMenuModel()
     {
-      MenuItems = new ItemsList();
+      MenuItems = new NavigationList<NestedItem>();
+      SubItems = new ItemsList();
       for (int i = 0; i < 10; i++)
       {
         NestedItem item = new NestedItem(Consts.KEY_NAME, "Main " + i);
@@ -60,7 +205,19 @@ namespace MediaPortal.UiComponents.RisingSkin.Models
         }
         MenuItems.Add(item);
       }
+      SetSubItems(MenuItems.Current);
       MenuItems.FireChange();
+      SetSelection(-1, 0);
+      MenuItems.OnCurrentChanged += SetSelection;
+    }
+
+    private void SetSelection(int oldindex, int newindex)
+    {
+      foreach (var nestedItem in MenuItems)
+      {
+        nestedItem.Selected = nestedItem == MenuItems.Current;
+      }
+      SetSubItems(MenuItems.Current);
     }
   }
 }
